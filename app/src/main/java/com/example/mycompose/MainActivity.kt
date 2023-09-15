d4aca875357b14f5db5cd8de28866aee1972e8b4
AndraBrata <komang.candra.brata@gmail.com>
2023-09-15 19:43:13 +0700

    example

--- app/src/main/java/com/example/mycompose/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.mycompose
 
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
@@ -16,14 +20,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mycompose.ui.theme.MyComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,6 +45,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -42,23 +54,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    Column (
+var count = remember {
+    mutableStateOf(0)
+}
+    val context = LocalContext.current
+    Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
 
+        modifier = Modifier.background(Color.Red)
     ) {
 
 
         Text(
-            text = "Hello $name!",
-            modifier = modifier.background(Color.Yellow),
-            color = Color.Blue
+            text = count.value.toString(),
+            modifier = modifier
+                .background(Color.Yellow)
+                .padding(10.dp),
+            color = Color.Blue,
+            fontSize = 30.sp
 
         )
 
-        Button(onClick = { /*TODO*/ }) {
-Image( imageVector = Icons.Default.Send, contentDescription = null)
+        Button(onClick = { /*TODO*/ count.value++
+        Toast.makeText(context,"${count.value}",Toast.LENGTH_SHORT).show()
+        }) {
+
+            Image( imageVector = Icons.Default.Send, contentDescription = null)
             Text(text = "Click")
         }
     }
